4da44d7f472cb7a51adcc39c3c054c4cdac47924
Bishaleyyyyy <techbishal566@gmail.com>
2025-08-01 19:49:50 +0545

    bug fixed

--- server/utils/imageUpload.ts
+++ server/utils/imageUpload.ts
@@ -1,16 +1,17 @@
-import { v2 as cloudinary } from "cloudinary";
-import streamifier from "streamifier";
+import { v2 as cloudinary } from 'cloudinary';
+import streamifier from 'streamifier';
 
-const uploadImageOnCloudinary = (file: Express.Multer.File) => {
+const uploadImageOnCloudinary = (file: Express.Multer.File): Promise<string> => {
   return new Promise((resolve, reject) => {
-    const uploadStream = cloudinary.uploader.upload_stream(
-      { folder: "restaurant-app" },
+    const stream = cloudinary.uploader.upload_stream(
+      { folder: 'restaurant-app' },
       (error, result) => {
-        if (error) reject(error);
-        else resolve(result?.secure_url);
+        if (error) return reject(error);
+        resolve(result?.secure_url || '');
       }
     );
-
-    streamifier.createReadStream(file.buffer).pipe(uploadStream);
+    streamifier.createReadStream(file.buffer).pipe(stream);
   });
 };
+
+export default uploadImageOnCloudinary;
